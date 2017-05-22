a5ed0be1cb909fcd81943e396d11af6a03cd0069
Sunday Adefila <adefilaedward@gmail.com>
2017-05-22 08:21:31 +0100

    rm pycache from git, add motions to root route

--- drive/urls.py
+++ drive/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+  url(r'^', include('motions.urls')),
   url(r'^motions/', include('motions.urls')),
   url(r'^admin/', admin.site.urls),
 ]
