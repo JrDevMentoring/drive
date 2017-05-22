0fc8613f8fcb7be0d87ba7c421dbbdcdcdfd4af1
Sunday Adefila <adefilaedward@gmail.com>
2017-05-22 08:24:37 +0100

    add users model to motions

--- motions/models.py
+++ motions/models.py
@@ -4,3 +4,6 @@ class Suggestion(models.Model):
   suggestion_text = models.CharField(max_length=500)
   votes = models.IntegerField(default=0)
   pub_date = models.DateTimeField('date published')
+
+class User(models.Model):
+  email: models.CharField
