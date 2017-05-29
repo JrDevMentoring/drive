a06ade22e002c822d33b05aee94e974c600c450b
Sunday Adefila <adefilaedward@gmail.com>
2017-05-29 14:12:36 +0100

    reconfigures static settings

--- drive/settings.py
+++ drive/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Get local settings from local_settings
--- drive/wsgi.py
+++ drive/wsgi.py
@@ -12,8 +12,6 @@ import os, sys
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-sys.path.append(os.path.join(BASE_DIR, 'drive'))
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drive.settings")
 
 application = get_wsgi_application()
